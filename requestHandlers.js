bf3cd192582461e24187a74ca44e520dbd7b3fe4
Sergiy Moskalenko <sergiyvm96@gmail.com>
2016-06-21 02:00:38 +0300

    Secon first commit

---
+++ requestHandlers.js
@@ -0,0 +1,24 @@
+var querystring = require("querystring");
+
+function start(response, postData) {
+    console.log("Request handler '/' was called.");
+    response.writeHead(301, {
+        Location: 'http://easysafety.esy.es'
+    });
+    response.end();
+}
+
+function locks(response, postData) {
+    console.log("Request handler 'locks' was called.");
+
+    var body = querystring.parse(postData).token;
+    response.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    response.write(body);
+    response.end();
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.locks = locks;
